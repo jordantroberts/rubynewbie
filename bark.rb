20717f5f4d7798e366eb22225957d44009456bab
Jordan <jordanroberts@talk21.com>
2019-03-18 21:28:36 +0000

    Final version of the Dog method using instance variables

--- bark.rb
+++ bark.rb
@@ -3,17 +3,19 @@ class Dog
   def bark
     return "Woof Woof"
   end
+
+  def colour= (colour) #define a  new method, 'colour ='. It takes one argument, which we named colour.
+    @colour = colour #saving the colour property, passed as an argument to the colour= method, to the object state.
+    #to save things to the object state, we use an instance variable.
+    #in line 2, we set up a new name @colour, and assign it to the colour parameter of the colour method
+  end
+
+  def observe
+    return "You see a " + @colour + " dog"
+  end
 end
 
 bailey = Dog.new
 puts bailey.bark
-
-
-class Cat
-  def miaow
-    return "Miaow! I want to kill Jordan, Miaow"
-  end
-end
-
-herbie = Cat.new
-puts herbie.miaow
+puts bailey.colour = "blue roan"
+puts bailey.observe
