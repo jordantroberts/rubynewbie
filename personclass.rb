2bef2ada4b84a2bb5a87234e2f14e54ced141dc9
Jordan <jordanroberts@talk21.com>
2019-03-18 21:39:44 +0000

    updated person class with explanation

--- personclass.rb
+++ personclass.rb
@@ -1,13 +1,16 @@
-class Person
-  def give_a_name(name)
-    @my_name = name
+class Person #define a Person class with some methods
+  def name=(name)
+    @name = name
   end
 
   def introduce
-    return "Hello, my name is " + @my_name
+    return "Hello, my name is " + @name
   end
 end
 
-woman = Person.new
-woman.give_a_name("Jordan")
+woman = Person.new #assign a new instance of Person to the variable 'woman'
+woman.name=("Jordan")
 puts woman.introduce
+
+#Methods that set object state, like 'name=' above are called SETTERS.
+#THEY SET OBJECT STATE USING AN INSTANCE VARIABLE
